c629b2750d9275927198946cf5dc0d48d1d33367
Matias Burd <matiasburd@hotmail.com>
2025-03-21 11:47:44 -0300

    Guardando cambios en entrega-final

--- src/components/pages/cart/Cart.jsx
+++ src/components/pages/cart/Cart.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router";
+
 const Cart = () => {
   return (
     <div>
       <h1>Acá va a ir el carrito</h1>
+      <Link to="/checkout">Finalizar compra</Link>
     </div>
   );
 };
--- src/components/pages/checkout/Checkout.jsx
+++ src/components/pages/checkout/Checkout.jsx
@@ -1,5 +1,56 @@
+import { useState } from "react";
+
 const Checkout = () => {
-  return <div>Checkout</div>;
+  const [userInfo, setUserInfo] = useState({
+    nombre: "",
+    apellido: "",
+    email: "",
+    telefono: "",
+  });
+
+  const funcionFormulario = (evento) => {
+    evento.preventDefault();
+    console.log(userInfo);
+    // En este momento es cuando nos conectamos con backend para realizar peticiones.
+  };
+
+  const funcionInputs = (evento) => {
+    const { value, name } = evento.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  return (
+    <div>
+      <form onSubmit={funcionFormulario}>
+        <input
+          type="text"
+          placeholder="Nombre"
+          name="nombre"
+          onChange={funcionInputs}
+        />
+        <input
+          type="text"
+          placeholder="Apellido"
+          name="apellido"
+          onChange={funcionInputs}
+        />
+        <input
+          type="text"
+          placeholder="Email"
+          name="email"
+          onChange={funcionInputs}
+        />
+        <input
+          type="text"
+          placeholder="Teléfono"
+          name="telefono"
+          onChange={funcionInputs}
+        />
+        <button>Enviar</button>
+        <button type="button">Cancelar</button>
+      </form>
+    </div>
+  );
 };
 
 export default Checkout;
